dc5ecaa764ea1721add3ad15d83fc6af32799bec
kinoru <xkinoru@gmail.com>
2016-02-10 20:27:33 +0900

    Add basic command-line argument processing

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,7 @@ import System.Exit
 import System.Environment
 
 import qualified Config as C
+import Args
 
 getHomePath :: IO FilePath
 getHomePath = lookupEnv "HOME" >>= maybe (die msgNoHome) return
@@ -16,5 +17,15 @@ getHomePath = lookupEnv "HOME" >>= maybe (die msgNoHome) return
 
 app :: IO ()
 app = do
+    cmd <- getCmd
+    case cmd of
+        CmdInit -> die "init not implemented yet"
+        CmdInstall _ -> die "install not implemented yet"
+        CmdRemove _ -> die "remove not implemented yet"
+        CmdUpgrade -> runUpgrade
+        CmdClean -> die "clean not implemented yet"
+
+runUpgrade :: IO ()
+runUpgrade = do
     conf <- (<> "/.fplug/fplug.yaml") <$> getHomePath >>= C.decode
     print (C.gitURLs conf)
