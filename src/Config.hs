a2725ba39aa36e13cc82f8a8fff9a4d0b6320af3
kinoru <xkinoru@gmail.com>
2016-02-10 17:49:16 +0900

    Add basic capability of reading fplug.yaml
    Read fplug.yaml and print out the Git URLs of the plugins.

---
+++ src/Config.hs
@@ -0,0 +1,37 @@
+module Config
+    ( decode
+    , encode
+    , Config(..)
+    , gitURLs
+    ) where
+
+import Data.Maybe
+import Data.Text (Text)
+import Data.Monoid
+
+import System.Exit
+
+import Data.Aeson.TH
+import qualified Data.Yaml as Y
+
+data Config = Config
+    { git :: Maybe [Text]
+    , github :: Maybe [Text]
+    }
+    deriving Show -- DEBUG
+
+deriveJSON defaultOptions ''Config
+
+decode :: FilePath -> IO Config
+decode path = Y.decodeFileEither path >>=
+    either (die . Y.prettyPrintParseException) return
+
+encode :: FilePath -> Config -> IO ()
+encode = Y.encodeFile
+
+gitURLs :: Config -> [Text]
+gitURLs (Config mgits mgithubs) = gits ++ githubURLs
+  where
+    gits = fromMaybe [] mgits
+    githubs = fromMaybe [] mgithubs
+    githubURLs = ["git@github.com:" <> url <> ".git" | url <- githubs]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,20 @@
 module Lib
-    ( someFunc
+    ( app
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Monoid
+
+import System.Exit
+import System.Environment
+
+import qualified Config as C
+
+getHomePath :: IO FilePath
+getHomePath = lookupEnv "HOME" >>= maybe (die msgNoHome) return
+  where
+    msgNoHome = "Environmental variable HOME is missing."
+
+app :: IO ()
+app = do
+    conf <- (<> "/.fplug/fplug.yaml") <$> getHomePath >>= C.decode
+    print (C.gitURLs conf)
