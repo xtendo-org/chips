7fa5acb08fffbecaf6e3003d1ee2fedad03441ef
kinoru <xkinoru@gmail.com>
2016-03-20 19:53:28 +0900

    Fix formatting of imported code

--- src/Spawn.hs
+++ src/Spawn.hs
@@ -12,14 +12,14 @@ type Result a = Either SomeException a
 
 spawnTry :: IO a -> IO (IO (Result a))
 spawnTry m = do
-  v <- newEmptyMVar
-  _ <- mask $ \restore -> forkIO (try (restore m) >>= putMVar v)
-  return (readMVar v)
+    v <- newEmptyMVar
+    _ <- mask $ \restore -> forkIO (try (restore m) >>= putMVar v)
+    return (readMVar v)
 
 spawn :: IO a -> IO (IO a)
 spawn m = do
-  r <- spawnTry m
-  return (r >>= either throwIO return)
+    r <- spawnTry m
+    return (r >>= either throwIO return)
 
 parMapIO :: (a -> IO b) -> [a] -> IO [b]
 parMapIO f xs = mapM (spawn . f) xs >>= sequence
