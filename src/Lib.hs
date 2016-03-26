3ba055144b3942fc620122b5374d833e1296556a
kinoru <xkinoru@gmail.com>
2016-03-27 05:43:24 +0900

    Restructure to add theme support

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,11 @@ module Lib
     ( app
     ) where
 
+import Data.Maybe
 import Data.Monoid
 import Control.Monad
 
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import Data.ByteString (ByteString)
@@ -24,7 +26,11 @@ import Spawn (parMapIO)
 import qualified Config as C
 import Git
 
-data Plugin = Plugin ByteString | Theme
+data Plugin = Plugin
+    { plugInit :: Maybe FilePath
+    , plugPrompt :: Maybe FilePath
+    , plugRightPrompt :: Maybe FilePath
+    }
 
 -- Session configuration container. In other words,
 -- things that are set up in the beginning of execution and hardly change
@@ -37,7 +43,7 @@ data Session = Session
 
 app :: IO ()
 app = do
-    B.putStrLn greetMsg
+    B.putStr greetMsg
     fpath <- getAppDirectory "chips"
     -- TODO: if plugin.yaml does not exist, create one with the template,
     -- and add "source" in config.fish
@@ -54,42 +60,50 @@ runSync Session{..} = do
     setCurrentDirectory pluginsDir
     -- Loop over each entry of plugin.yaml
     plugResults <- parMapIO (installPlug pluginsExist) $ C.gitURLs chipsConf
-    withFile buildPath WriteMode $ \handle ->
-        B.hPutBuilder handle $ mconcat $ sourcePaths plugResults
+    withFile buildPath WriteMode $ \handle -> B.hPutBuilder handle $
+        mconcat $ map sourceInit $ mapMaybe plugInit plugResults
     bPutStr $ "Build result saved at " <> B.stringUtf8 buildPath <> "\n"
   where
     pluginsDir = chipsPath </> "dist"
     buildPath = chipsPath </> "build.fish"
-    installPlug pluginsExist url = do
-        pluginDirExist <- if not pluginsExist
-            then return False
-            else doesDirectoryExist $ B.unpack dir
-        if pluginDirExist then
-            bPutStr $ B.byteString dir <> " is already installed.\n"
-            -- TODO: do the update here
-        else do
-            let builderDir = B.byteString dir
-            bPutStr $ "Installing " <> builderDir <> "...\n"
-            cloned <- silentCall
-                "git" ["clone", "--depth=1", T.unpack url, B.unpack dir]
-            case cloned of
-                ExitSuccess -> bPutStr $ "Installed " <> builderDir <> ".\n"
-                _ -> B.putStrLn "Fail."
-        doesFileExist (B.unpack dir </> "init.fish") >>= \case
-            True -> return (Plugin dir)
-            False -> return Theme
-      where
-        dir = maybe (B64.encode $ T.encodeUtf8 url) T.encodeUtf8 (gitDir url)
-    sourcePaths [] = []
-    sourcePaths (p : ps) = case p of
-        Plugin dir -> pathExpand dir : sourcePaths ps
-        Theme -> sourcePaths ps
-      where
-        pathExpand dir = mconcat
-            [ "source "
-            , B.stringUtf8 $ pluginsDir </> B.unpack dir </> "init.fish"
-            , "\n"
-            ]
+    sourceInit initPath = mconcat
+        [ "source "
+        , B.stringUtf8 $ pluginsDir </> initPath
+        , "\n"
+        ]
+
+installPlug :: Bool -> Text -> IO Plugin
+installPlug pluginsExist url = do
+    plugDirExists <- if not pluginsExist
+        then return False
+        else doesDirectoryExist dir
+    if plugDirExists then
+        bPutStr $ builderDir <> " is already installed.\n"
+        -- TODO: do the update here
+    else do
+        bPutStr $ "Installing " <> builderDir <> "...\n"
+        cloned <- silentCall
+            "git" ["clone", "--depth=1", T.unpack url, dir]
+        bPutStr $ (<> builderDir <> ".\n") $ case cloned of
+            ExitSuccess -> "Installed "
+            _ -> "Failed installing "
+    initExists <- doesFileExist initFishPath
+    promptExists <- doesFileExist promptPath
+    rightExists <- doesFileExist rightPath
+    return Plugin
+        { plugInit = if initExists then Just initFishPath else Nothing
+        , plugPrompt = if promptExists then Just promptPath else Nothing
+        , plugRightPrompt = if rightExists then Just rightPath else Nothing
+        }
+  where
+    dir :: FilePath
+    dir = B.unpack $
+        maybe (B64.encode $ T.encodeUtf8 url) T.encodeUtf8 (gitDir url)
+    builderDir :: Builder
+    builderDir = B.stringUtf8 dir
+    initFishPath = dir </> "init.fish"
+    promptPath = dir </> "fish_prompt.fish"
+    rightPath = dir </> "fish_right_prompt.fish"
 
 -- utility
 
@@ -106,6 +120,4 @@ bPutStr :: Builder -> IO ()
 bPutStr = B.hPutBuilder stdout
 
 greetMsg :: ByteString
-greetMsg = "\
-  \chips: fish plugin manager\
-\\n=========================="
+greetMsg = "chips: fish plugin manager\n\n"
