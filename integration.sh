2686796f9728aa82286e73b846cf5a0a49f18494
kinoru <xkinoru@gmail.com>
2016-05-29 12:35:33 +0900

    Test: bold text for test output

--- integration.sh
+++ integration.sh
@@ -1,18 +1,23 @@
-echo ">>> Preparing integration test"
-stack install > /dev/null 2>&1 || exit 1
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "$bold>>> Preparing integration test$normal"
+stack install > /dev/null || exit 1
 docker build -t kinoru/chips integration > /dev/null || exit 1
 cp ~/.local/bin/chips ./integration
-echo ">>> Test start"
+
+echo "$bold>>> Test start$normal"
 docker run -a stdout -t --name chips-integration-test -v $(pwd $(dirname $0))/integration:/home/user/integration kinoru/chips /usr/bin/bash /home/user/integration/test.sh
 rm ./integration/chips
 docker stop chips-integration-test > /dev/null
 docker rm chips-integration-test > /dev/null
+
 export RESULT=$(cat ./integration/result.txt)
 rm ./integration/result.txt
 if test "$RESULT" = "success"; then
-    echo ">>> Test success"
+    echo "$bold>>> Test success$normal"
     exit 0
 else
-    echo ">>> Test fail"
+    echo "$bold>>> Test fail$normal"
     exit 1
 fi
